72194bf5a546ab3c208ce15e91d52dfe97ef2870
Irina Merkulova <irina.merkulova@tthk.ee>
2021-11-10 09:52:39 +0200

    Mõistatus

---
+++ harjutused/vastus.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Matemaatika vastused</title>
+</head>
+<body>
+<?php
+// lisada info teisest failist
+require_once ('moistatus.php');
+echo "<br>Õiged vastused olid: ";
+echo "arv 1 on ".$arv1;
+echo " ja arv 2 on ".$arv2;
+?>
+
+
+</body>
+</html><?php
